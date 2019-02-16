0c9be682282ba8630785049f4dad41dd8c346c33
seeni <seeni0424@gmail.com>
2019-02-16 22:21:41 +0530

    add config file

---
+++ config_port_address.py
@@ -0,0 +1,16 @@
+'''Following are the constants for connecting to master'''
+MASTER_UDP_ADDRESS="localhost"
+
+#machine-protocol-action-component
+
+#master listening
+MASTER_UDP_LISTEN_CLIENT_PORT=9000
+MASTER_UDP_LISTEN_SLAVE_PORT = 9001
+
+MASTER_UDP_SEND_CLIENT_PORT=9002
+MASTER_UDP_SEND_SLAVE_PORT = 9003
+
+#clients should be using less number of ports as much as possible
+CLIENT_UDP_SEND_PORT=9001
+CLIENT_UDP_RECEIVE_PORT=9002
+
